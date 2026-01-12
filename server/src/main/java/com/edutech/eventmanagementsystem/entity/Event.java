160f87b263a3341c5d16a02a86b0df838778937e
A-d-e-e <dummy@gmail.com>
2026-01-12 04:57:35 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/eventmanagementsystem/entity/Event.java
@@ -0,0 +1,111 @@
+package com.edutech.eventmanagementsystem.entity;
+
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Table(name = "events") // do not change table name
+public class Event {
+    // implement entity
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eventId;
+    private String title;
+    private String description;
+    private  Date dateTime;
+    private String location;
+    private  String status;
+    private List<Allocation> allocations;
+
+
+    public Event() {
+    }
+
+
+    public Event(Long eventId, String title, String description, Date dateTime, String location, String status,
+            List<Allocation> allocations) {
+        this.eventId = eventId;
+        this.title = title;
+        this.description = description;
+        this.dateTime = dateTime;
+        this.location = location;
+        this.status = status;
+        this.allocations = allocations;
+    }
+
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+
+    public String getLocation() {
+        return location;
+    }
+
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    public List<Allocation> getAllocations() {
+        return allocations;
+    }
+
+
+    public void setAllocations(List<Allocation> allocations) {
+        this.allocations = allocations;
+    }
+
+    
+    
+
+   
+}

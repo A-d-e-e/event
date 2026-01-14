2415f14f5eb05bfa6dd4f6d3aced17bb4f50386f
A-d-e-e <dummy@gmail.com>
2026-01-14 11:46:41 +0000

    Pushed for testcases

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -22,9 +22,9 @@ const routes: Routes = [
   { path: 'resource-allocate', component: ResourceAllocateComponent },  
   { path: 'view-events', component: ViewEventsComponent },  
   { path: 'booking-details', component: BookingDetailsComponent },   
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 
-  { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
